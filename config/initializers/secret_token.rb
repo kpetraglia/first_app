7c032b5d40565d81a17223f4e7b0ee22eb191903
Kara Petraglia <kara.petraglia@fracturedatlas.org>
2011-06-02 17:12:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '541c20f1963b57c265e7ee73931dd9bfa56b27aea1ca91b6e20551cca1ceb3647686e7356cf044d32f74c5edffbe09257d20b3d1e52be11ec7cf0006a9dfbe87'
